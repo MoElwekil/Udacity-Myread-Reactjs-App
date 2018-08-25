334ff712f5a2f01885ffacf84130025481f10720
Mo <Mo@descod.net>
2018-08-25 08:20:24 +0100

    The Search page was created and passed through React Router Dom

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import './App.css'
 
 // import BooksGrid
 import BooksGrid from './BooksGrid'
+// import Search
+import Search from './Search'
+// import Route from React Router
+// import React Router Dom as Link
+import { Route, Link } from 'react-router-dom'
+
 
 class BooksApp extends React.Component {
 	state = {
-		showSearchPage: false,
+		screen: 'app',
 		books: []
 	}
 
@@ -29,32 +35,18 @@ class BooksApp extends React.Component {
 	render() {
 		return (
 			<div className="app">
-				{this.state.showSearchPage ? (
-					<div className="search-books">
-						<div className="search-books-bar">
-							<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-							<div className="search-books-input-wrapper">
-								{/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-								<input type="text" placeholder="Search by title or author" />
-
-							</div>
-						</div>
-						<div className="search-books-results">
-							<ol className="books-grid"></ol>
+				<Route exact path="/" render={() => (
+					<div>
+						<BooksGrid books={this.state.books} MoveShelfs={this.MoveShelfs} />
+						<div className="open-search">
+							<Link to='/search' onClick={() => this.setState({ screen: 'search' })}>Add a book</Link>
 						</div>
 					</div>
-				) : (
-						<div>
-							<BooksGrid books={this.state.books} MoveShelfs={this.MoveShelfs} />
-						</div>
-					)}
+				)} />
+
+				<Route path="/search" render={() => (
+					<Search />
+				)} />
 			</div>
 		)
 	}
